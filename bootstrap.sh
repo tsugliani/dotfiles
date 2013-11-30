fd7889ab43b8a06dcd1f8ed063aa55d47c3fad0b
Timo Sugliani <timo.sugliani@gmail.com>
2013-12-01 00:23:48 +0100

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,23 @@
 
 dotfiles=$HOME/.dotfiles
 
-
 echo "Bootstrap $dotfiles"
 
-# ZSH Config
+# BASH Config
+ln -s $dotfiles/bash/bash_prompt $HOME/.bash_prompt
 
+# ZSH Config
 ln -s $dotfiles/zsh/zshrc $HOME/.zshrc
 ln -s $dotfiles/zsh/oh-my-zsh $HOME/.oh-my-zsh
 
-
 # TMUX Config
-
 ln -s $dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
-
 # VIM Config
-
 ln -s $dotfiles/vim/vimrc $HOME/.vimrc
+
+# GIT config
+ln -s $dotfiles/git/gitconfig $HOME/.gitconfig
+ln -s $dotfiles/git/gitignore_global $HOME/.gitignore_global
+
 
